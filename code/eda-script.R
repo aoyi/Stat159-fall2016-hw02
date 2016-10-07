d4dfdd21e5f57730bac21a466e96872d66842258
Aoyi <aoyi95@berkeley.edu>
2016-10-06 17:42:12 -0700

    Change eda-output code

--- code/eda-script.R
+++ code/eda-script.R
@@ -1,9 +1,34 @@
 
 data_set <- read.csv("data/Advertising.csv")
 
+output_stats <- function(data, name, file) {
+  data_min <- min(data)
+  data_max <- max(data)
+  data_median <- median(data)
+  data_avg <- mean(data)
+  data_sd <- sd(data)
+  cat("Summary Statistics for", name, "\n\n", file = file, append = TRUE)
+  cat(sprintf("Minimum: %0.2f", data_min), "\n", file = file, append = TRUE)
+  cat(sprintf("Maximum: %0.2f", data_max), "\n", file = file, append = TRUE)
+  cat(sprintf("Median : %0.2f", data_median), "\n", file = file, append = TRUE)
+  cat(sprintf("Mean   : %0.2f", data_avg), "\n", file = file, append = TRUE)
+  cat(sprintf("Std Dev: %0.2f", data_sd), "\n\n", file = file, append = TRUE)
+}
+
 sink(file = "data/eda-output.txt")
-summary(data_set)
+output_stats(data_set$TV, "TV", "data/eda-output.txt")
+output_stats(data_set$Sales, "Sales", "data/eda-output.txt")
 sink()
+
+
+
+
+
+
+
+
+
+
 
 png("images/histogram-tv.png")
 hist(data_set$TV, main = "Histogram of TV Advertising", xlab = "TV Advertising", col = "blue")
